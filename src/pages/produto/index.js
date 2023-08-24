c8eccadda6d355c23f469f7c03c351e23ca6711d
igao <nascimentoigor1311@gmail.com>
2023-08-24 19:35:25 -0300

    pagina produto - secao produto e relacionados

--- src/pages/produto/index.js
+++ src/pages/produto/index.js
@@ -3,17 +3,18 @@ import './index.scss';
 import Cabecalho from '../../components/cabecalho';
 import Rodape from '../../components/rodape';
 import CabecalhoLog from '../../components/cabecalho(logado)';
+import CardProduto from '../../components/cardProduto';
 
 import { Link } from 'react-router-dom/dist';
 
 export default function Pedido() {
     return(
         <div className="pagina-produto">
-            <Cabecalho/>
+            <CabecalhoLog/>
             
             <div className="container-main">
                 <main>
-                    <div className="container-produto">
+                    <section className="container-produto">
                         <div className="imagens">
                             <div className="cont-imagem-principal">
                                 <img src="/assets/images/foto_produto.png" alt="" />
@@ -42,9 +43,9 @@ export default function Pedido() {
                                     <h4>Ou em até 10x</h4>
                                 </div>
 
-                                <div className='linha'></div>
-
-                                <h3>33% de Desconto</h3>
+                                <div className='desconto'>
+                                    <h3>33% de Desconto</h3>
+                                </div>
                             </div>
 
                             <Link>Ver os meios de pagamento</Link>
@@ -63,9 +64,42 @@ export default function Pedido() {
                         </div>
 
                         <div className="entrega">
-                            
+                            <h2>R$ 29,99</h2>
+                            <hr/>
+
+                            <div className="input-cep">
+                                <h3>Descobrir formas de entrega</h3>
+                                <input type="text" placeholder="Digite seu CEP"/>
+                            </div>
+
+                            <div className="container-entregas">
+                                <h2>Entrega mais Rápida</h2>
+
+                                <div>
+                                    <img src="/assets/images/loggiLogo.png" alt="" />
+                                    <div>
+                                        <h4>Entrega Loggi</h4>
+                                        <p>Receba em até 2 dias úteis</p>
+                                        <h5>R$ 10.99</h5>
+                                    </div>
+                                </div>
+                            </div> 
+
+                            <button className="btn-comprar">Comprar agora</button>
+                            <button className="btn-carrinho">Adicionar ao carrinho</button> 
                         </div>
-                    </div>
+                    </section>
+
+                    <section className="sec-relacionados">
+                        <h1>Relacionados</h1>
+
+                        <div>
+                            <CardProduto tema="tema-cinza"/>
+                            <CardProduto tema="tema-cinza"/>
+                            <CardProduto tema="tema-cinza"/>
+                            <CardProduto tema="tema-cinza"/>
+                        </div>
+                    </section>
                 </main>
             </div>
 
