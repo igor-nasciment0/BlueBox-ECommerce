85e6b62e87da4d86c654088228bd1e257656939f
igao <nascimentoigor1311@gmail.com>
2023-10-04 15:19:32 -0300

    cards responsivos tela inicial

--- src/components/cardProduto/index.js
+++ src/components/cardProduto/index.js
@@ -1,4 +1,4 @@
-import { useActionData } from 'react-router-dom';
+import { Link, useActionData, useNavigate } from 'react-router-dom';
 import './index.scss';
 import { useContext, useState } from 'react';
 import { TemaContext } from '../../theme';
@@ -7,9 +7,11 @@ export default function CardProduto(props)
 {
     let context = useContext(TemaContext);
     let tema = context.tema;
+
+    const navigate = useNavigate();
     
     return(
-        <div className={'card-produto ' + tema}>
+        <div className={'card-produto ' + tema} onClick={navigate('/produto')}>
             <img src='/assets/images/foto_produto.png' alt="Assassin's Creed" />
             <h2>Assassins Creed Brotherhood (Seminovo)</h2>
             <h4 className='preco-anterior'>R$ 44,99</h4>
@@ -18,7 +20,7 @@ export default function CardProduto(props)
             <p>Ou em até 10x de R$2,99</p>
             <p>PIX: 10% de Desconto</p>
 
-            <a href="">Comprar</a>
+            <Link href="">Comprar</Link>
         </div>
     )
 }
