f4fc11429196e83b905f8a30cc2a599362d6386c
Alvxss <alvxss20@gmail.com>
2023-10-10 17:17:41 -0300

    ok

---
+++ src/pages/ADM/pedidosConcluidos/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+import CabecalhoADM from '../../../components/ADM/cabecalho';
+import BarraLateral from '../../../components/ADM/barraLateral';
+
+export default function PedidoConcluido() {
+  return (
+    <div className='pedidoConcluido'>
+      
+    </div>
+  )
+}
--- src/router.js
+++ src/router.js
@@ -14,9 +14,11 @@ import Perfil from './pages/perfil';
 import LoginAdm from './pages/ADM/loginAdm';
 import CadastroProduto from './pages/ADM/cadastroProduto';
 import ConsultaProduto from './pages/ADM/consultaProduto';
+import PedidoConcluido from './pages/ADM/pedidosConcluidos';
 import { useContext, useState } from 'react';
 import { TemaContext } from './theme';
 import Contato from './pages/contato';
+import PedidoConcluido from './pages/ADM/pedidosConcluidos';
 
 export default function Router() {
 
@@ -53,7 +55,8 @@ export default function Router() {
 
                     <Route path='/adm/login' element={<LoginAdm/>}/>
                     <Route path='/adm/cadastro-produto' element={<CadastroProduto/>}/>
-                    <Route path='/adm/consulta-produto' element={<ConsultaProduto/>}/>   
+                    <Route path='/adm/consulta-produto' element={<ConsultaProduto />} />   
+                    <Route path='/adm/pedido-concluido' element={<PedidoConcluido />} />
                 </Routes>
             </BrowserRouter>
         </TemaContext.Provider>
