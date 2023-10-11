bb531aa40d66a196fea3392e7dd282362bc15e1c
HenriSilva11 <pequenohenri72@gmail.com>
2023-10-11 17:33:38 -0300

    responsividdade

--- src/pages/Tela-dados-cartão/index.js
+++ src/pages/Tela-dados-cartão/index.js
@@ -66,7 +66,7 @@ export default function Telacartao() {
               </div>
 
               <button className="finalizar-pedido">
-                Finalizar pedido
+                FINALIZAR PEDIDO
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="21"
