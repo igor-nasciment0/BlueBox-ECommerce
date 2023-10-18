7d59043d12605b016e711cd3250c0cfa2b782f49
Alvxss <alvxss20@gmail.com>
2023-10-18 17:47:30 -0300

    ok

--- src/pages/ADM/pedidosConcluidos/index.js
+++ src/pages/ADM/pedidosConcluidos/index.js
@@ -9,10 +9,11 @@ export default function PedidoConcluido() {
       <div className='s1'>
         <BarraLateral />
         <div className='conteudo'>
-          <h1></h1>
+          <h1>Pedidos Concluídos</h1>
           <div className='ordenar'>
             <div>
               <p>Ordenar de A a Z:</p>
+              <img src="/assets/images/icons/adm/setaBaixoPreta.svg" alt="SetaBaixo" />
             </div>
           </div>
           <div className='pedidos-concluidos'>
@@ -21,37 +22,197 @@ export default function PedidoConcluido() {
                 <p>Produto</p>
                 <h6>The Last of Us - PS3</h6>
               </div>
-            </div>
-            <div className='pedidos'>
               <div>
                 <p>Qtd.</p>
                 <h6>1</h6>
               </div>
-            </div>
-            <div className='pedidos'>
               <div>
                 <p>Comprador</p>
                 <h6>Bruno Fofo</h6>
               </div>
-            </div>
-            <div className='pedidos'>
               <div>
                 <p>Data do Pedido</p>
                 <h6>15/05/2023</h6>
               </div>
-            </div>
-            <div className='pedidos'>
               <div>
                 <p>Data de Entrega</p>
                 <h6>20/05/2023</h6>
               </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
             </div>
-            <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
-            <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
+            <div className='pedidos'>
+              <div>
+                <p>Produto</p>
+                <h6>The Last of Us - PS3</h6>
+              </div>
+              <div>
+                <p>Qtd.</p>
+                <h6>1</h6>
+              </div>
+              <div>
+                <p>Comprador</p>
+                <h6>Bruno Fofo</h6>
+              </div>
+              <div>
+                <p>Data do Pedido</p>
+                <h6>15/05/2023</h6>
+              </div>
+              <div>
+                <p>Data de Entrega</p>
+                <h6>20/05/2023</h6>
+              </div>
+              <img src="/assets/images/icons/adm/BotãoOlhar.svg" alt="Olho" />
+              <img src="/assets/images/icons/adm/concluido.svg" alt="Certo" />
+            </div>
           </div>
           <div className='tudo'>
             <div className='verTudo'>
-              <a href=""></a>
+              <a href="">Ver Tudo</a>
             </div>
           </div>
         </div>
