c19e2a14cb736d91d12ebb185f0cfd1cd48dfb0f
HenriSilva11 <pequenohenri72@gmail.com>
2023-10-24 14:48:15 -0300

    okok

---
+++ src/pages/ADM/graficoReceita/index.js
@@ -0,0 +1,13 @@
+import BarraLateral from "../../../components/ADM/barraLateral";
+import CabecalhoADM from "../../../components/ADM/cabecalho";
+
+export default function Graficoreceita() {
+  return (
+    <div className="grafico-receita">
+      <CabecalhoADM />
+      <main>
+        <BarraLateral />
+      </main>
+    </div>
+  );
+}
