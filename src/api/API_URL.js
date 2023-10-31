a1d62a5d002c74a5096ba22722a389a2a856162c
Alvxss <alvxss20@gmail.com>
2023-10-31 14:13:46 -0300

    okok

--- src/api/API_URL.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL: 'http://129.148.42.252:5033'
-});
-
-/* export const api = axios.create({
-    baseURL: 'http://localhost:5033'
-}); */
